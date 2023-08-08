c46f96377780e2d454effb9f927ebf121e89957e
HIJIOKA Shinji <l.hill.webdesign@gmail.com>
2023-08-08 17:38:12 +0900

    all done

---
+++ main.js
@@ -0,0 +1,142 @@
+import "./style.css";
+import * as THREE from "three";
+ 
+//canvas
+const canvas = document.querySelector("#webgl");
+ 
+//シーン
+const scene = new THREE.Scene();
+
+//背景用のテクスチャ
+const textureLoader = new THREE.TextureLoader();
+const bgTexture = textureLoader.load("./images/scene-bg.jpg");
+scene.background = bgTexture;
+ 
+//サイズ
+const sizes = {
+  width: innerWidth,
+  height: innerHeight,
+};
+ 
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+ 
+//レンダラー
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+//オブジェクトを作成
+const boxGeometry = new THREE.BoxGeometry(5, 5, 5, 10);
+const boxMaterial = new THREE.MeshNormalMaterial();
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(0 , 0.5, -15);
+box.rotation.set(1, 1, 0);
+
+
+const torusGeometry = new THREE.TorusGeometry(8, 2, 16, 100);
+const torusMaterial = new THREE.MeshNormalMaterial();
+const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+torus.position.set(0 , 1, 10);
+
+scene.add(box, torus);
+
+//線形補間で滑らかに移動させる
+function lerp(x, y, a) {
+  return (1-a) * x + a * y;
+}
+
+function scalePercent(start, end) {
+  return (scrollParcent - start) / (end - start);
+}
+
+//スクロールアニメーション
+const animationScripts = [];
+
+animationScripts.push({
+  start: 0,
+  end: 40,
+  func() {
+    camera.lookAt(box.position);
+    camera.position.set(0, 1, 10);
+    box.position.z = lerp(-15, 2, scalePercent(this.start, this.end))
+    torus.position.z = lerp(10, -20, scalePercent(this.start, this.end))
+  }
+});
+
+animationScripts.push({
+  start: 40,
+  end: 60,
+  func() {
+    camera.lookAt(box.position);
+    camera.position.set(0, 1, 10);
+    box.rotation.z = lerp(1, Math.PI * 2, scalePercent(this.start, this.end));
+  }
+});
+
+animationScripts.push({
+  start: 60,
+  end: 80,
+  func() {
+    camera.lookAt(box.position);
+    camera.position.set(0, 1, 10);
+    camera.position.x = lerp(0, -15, scalePercent(60, 80));
+    camera.position.y = lerp(1, 15, scalePercent(60, 80));
+    camera.position.z = lerp(10, 25, scalePercent(60, 80));
+  }
+});
+
+animationScripts.push({
+  start: 80,
+  end: 100,
+  func() {
+    camera.lookAt(box.position);
+    box.rotation.x += 0.02;
+    box.rotation.y += 0.02;
+  }
+});
+
+//ブラウザのスクロール率を取得
+let scrollParcent = 0;
+document.body.onscroll = () => {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight;
+  let browserHeight = document.documentElement.clientHeight
+  scrollParcent = ( scrollTop / (scrollHeight - browserHeight)) * 100;
+}
+
+function playScrollAnimation() {
+  animationScripts.forEach(a => {
+    if(scrollParcent >= a.start && scrollParcent <= a.end) {
+      a.func();
+    }
+  })
+}
+
+//アニメーション
+const tick = () => {
+  window.requestAnimationFrame(tick);
+  playScrollAnimation();
+  renderer.render(scene, camera);
+};
+ 
+tick();
+ 
+//ブラウザのリサイズ操作
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+ 
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+ 
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
